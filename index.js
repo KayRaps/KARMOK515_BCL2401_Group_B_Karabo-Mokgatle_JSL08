05fa6bbc872d79165b12f774f1167d8300fe935b
Karabo Edward Mokgatle <karaboedward64@gmail.com>
2024-04-10 09:58:33 +0200

    Just deleted some instructions, that are already in use.

--- index.js
+++ index.js
@@ -25,16 +25,9 @@ constructor(branchInfo) {
     return bankBranchInstance;
 }
 
-//- If `bankBranchInstance` is null, create a new instance with the provided `bra
 
 
 }
-
-// 3. In the `BankBranch` class:
-//    - Create a constructor that takes `branchInfo` as a parameter.
-//    - Inside the constructor, check if the `bankBranchInstance` variable is null (indicating no instance exists).
-//    - If `bankBranchInstance` is null, create a new instance with the provided `branchInfo` and assign it to `bankBranchInstance`.
-//    - Return the `bankBranchInstance` whether it's newly created or existing.
 
 // 4. Add methods to the `BankBranch` class for managing branch-related information. For example, you can add a method like `getBranchInfo` to retrieve branch details.
 
